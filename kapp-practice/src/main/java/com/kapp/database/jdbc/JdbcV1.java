8470e321534493fd856d59a47c820e061b1a34bd
y022 <y022586@126.coom>
2024-03-08 18:18:37 +0800

    修改数据库地址

--- kapp-practice/src/main/java/com/kapp/database/jdbc/JdbcV1.java
+++ kapp-practice/src/main/java/com/kapp/database/jdbc/JdbcV1.java
@@ -1,9 +1,38 @@
 package com.kapp.database.jdbc;
 
+import org.postgresql.Driver;
+
+import java.sql.*;
+
 public class JdbcV1 {
-    public static void main(String[] args) throws ClassNotFoundException {
-        Class<?> pgClass = Class.forName("");
-//        jdbc:postgresql://140.210.220.31:5432/postgres
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        String url = "jdbc:postgresql://140.210.220.31:5432/postgres";
+
+        String user = "postgres";
+
+        String pd = "y1039390833";
+
+        //注册驱动
+        DriverManager.registerDriver(new Driver());
+
+        //获取数据库连接
+        Connection connection = DriverManager.getConnection(url, user, pd);
+
+        String sql = "select * from table_control_valve";
+
+        //通过连接执行sql
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        preparedStatement.execute();
+
+        ResultSet resultSet = preparedStatement.getResultSet();
+
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("id"));
+        }
+
 
     }
 }
