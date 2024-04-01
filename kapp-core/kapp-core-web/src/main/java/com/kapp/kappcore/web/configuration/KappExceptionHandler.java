6b71488b1b7edfaac93ae968f04509c381aceeaf
抢不到人机就哭 <58139841+y022@users.noreply.github.com>
2024-04-01 23:00:39 +0800

    异常拦截器优化

--- kapp-core/kapp-core-web/src/main/java/com/kapp/kappcore/web/configuration/KappExceptionHandler.java
+++ kapp-core/kapp-core-web/src/main/java/com/kapp/kappcore/web/configuration/KappExceptionHandler.java
@@ -2,6 +2,7 @@ package com.kapp.kappcore.web.configuration;
 
 import com.kapp.kappcore.ApiResponse;
 import com.kapp.kappcore.constant.ExCode;
+import com.kapp.kappcore.exception.SearchException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,5 +18,14 @@ public class KappExceptionHandler {
                 .builder()
                 .code(ExCode.error)
                 .message("系统错误").build();
+    }
+
+    @ExceptionHandler(SearchException.class)
+    public Object searchExceptionHandler(SearchException exception) {
+        log.error("searchException", exception);
+        return ApiResponse
+                .builder()
+                .code(exception.getExceptionCode())
+                .message(exception.getExceptionCode()).build();
     }
 }
